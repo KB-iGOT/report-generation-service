df5f95c08d8220ca557cd6e4b86216aaa7e88279
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-05-13 17:33:42 +0530

    adding the test

--- tests/controllers/test_report_controller_v2.py
+++ tests/controllers/test_report_controller_v2.py
@@ -10,6 +10,7 @@ from app.controllers.report_controller_v2 import report_controller_v2
 def app():
     """Create a Flask test app with the report_controller blueprint registered."""
     app = Flask(__name__)
+    app.config['WTF_CSRF_ENABLED'] = False
     app.register_blueprint(report_controller_v2)
     app.config['TESTING'] = True
     return app
