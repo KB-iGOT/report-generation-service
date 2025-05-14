6dd9f4d85bd7ad59394855f5ce9b82d82dff6254
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-05-14 18:05:44 +0530

    updating the code

--- app/controllers/report_controller_v2.py
+++ app/controllers/report_controller_v2.py
@@ -13,8 +13,7 @@ from constants import X_AUTHENTICATED_USER_TOKEN, IS_VALIDATION_ENABLED, X_ORG_I
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    force=True
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
 
